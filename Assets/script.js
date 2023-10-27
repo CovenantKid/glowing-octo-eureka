a24eaf06face87ae7a9b6d46118fef795fb638db
Brad <Bradrockk@gmail.com>
2023-10-27 11:12:40 -0400

    finished code for saving text to specific ID as key

--- Assets/script.js
+++ Assets/script.js
@@ -3,11 +3,9 @@ $(document).ready(function(){
   const CurrentTime = document.getElementById("currentDay");
   CurrentTime.textContent = dayjs().format("dddd h:mmA MMM DD");
 
-
 // grab current hour
 var hour = dayjs().format("HH");
 console.log("current hour is",hour);
-
 
 // change class of time-blocks based on hour 
 // first interate through all items with class of time-block.
@@ -33,43 +31,20 @@ console.log("current hour is",hour);
     i.addEventListener("click",save,this)
   }
 
-
 });
 
 // in the save function, we first get the parent node id we are saving to.
 // we use this to identify the text block that will be saved and where. 
 function save(){
-  // grabbing parent node
- var parent = this.parentNode;
-//  grabbing just the id of the parent node
- var parentID = parent.id;
+  // grabbing parent nodes ID
+ var parent = this.parentNode.id;
 // grabbing the text from the typed field
-var parentTextArea = document.getElementById("09text").value
-console.log(parentTextArea)
-var saved = localStorage.setItem(parentID,"hello")
-
-
+var parentTextArea = document.getElementById(parent + "text").value
+// save the text using the parents ID, and the text entered by the user.
+var saved = localStorage.setItem(parent,parentTextArea)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
