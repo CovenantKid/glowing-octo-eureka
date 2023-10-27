bf1d38bb6db4e74e1d53fb8010edabbc16eb21e0
Brad <Bradrockk@gmail.com>
2023-10-27 12:08:43 -0400

    added code that loads text based on keys

--- Assets/script.js
+++ Assets/script.js
@@ -30,7 +30,20 @@ console.log("current hour is",hour);
   for( i of saveButtons){
     i.addEventListener("click",save,this)
   }
+  // load function will pull saved text and keys in proper position on page.
+  load()
+function load(){
+for (i = 0; i < localStorage.length; i++){
+  // grabbing all saved keys 
+var currentKey = localStorage.key(i)
+// grabbing the saved text from the key
+var currentKeyText = localStorage.getItem(currentKey)
+// grabbing the id equivelent of the key from part 1. This will be used to write the text that belongs to that time block.
+var currentID = document.getElementById(currentKey + "text")
+// setting the text content of the block to the saved text from the key.
+currentID.textContent = currentKeyText
 
+}}
 });
 
 // in the save function, we first get the parent node id we are saving to.
@@ -43,10 +56,3 @@ var parentTextArea = document.getElementById(parent + "text").value
 // save the text using the parents ID, and the text entered by the user.
 var saved = localStorage.setItem(parent,parentTextArea)
 }
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
